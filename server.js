2b3f9996f5334d9a1efabd8063510fc7845b702b
marc vargas <marcvargas@marcs-Air.attlocal.net>
2023-08-21 13:55:07 -0500

    added  main menu question

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
-console.log('hello world')
+const inquirer = require('inquirer')
+
+const menu = [
+    {
+        type: 'list', 
+        message: 'What would you like to do?',
+        name: 'command',
+        choices: [
+            'View All Employees',
+            'Add Employee',
+            'Update Employee Role',
+            'View All Roles',
+            'Add Role',
+            'View All Departments',
+            'Add Department',
+            'Quit'
+        ]
+    }
+]
+
+inquirer.prompt(menu).then((answers) => {
+    console.log(answers)
+})
+
